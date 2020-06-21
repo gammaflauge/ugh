b1561034fed4ba125c5930ae4f1d7b090b30a818
gammaflauge <mddevine@gmail.com>
2020-06-20 23:31:55 -0400

    :tada: initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import json
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+# https://redcap.chop.edu/redcap_v9.3.4/index.php?pid=22810
+redcap_token = os.environ.get("REDCAP_TOKEN_UGH")
+assert len(redcap_token) == 32
+
+post_parameters = {
+    "token": redcap_token,
+    "content": "record",
+    "format": "json",
+    "type": "flat",
+    "rawOrLabel": "raw",
+    "rawOrLabelHeaders": "raw",
+    "exportCheckboxLabel": "false",
+    "exportSurveyFields": "false",
+    "exportDataAccessGroups": "false",
+    "returnFormat": "json",
+}
+
+
+@app.route("/", methods=["GET"])
+def home():
+    r = requests.post("https://redcap.chop.edu/api/", data=post_parameters)
+    issues = json.loads(r.content)
+    return render_template("index.html", issues=issues)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,13 @@
+# ugh
+Ultimate Glitch History for the CHOP On-call team.
+
+### setup
+```console
+> python -m venv venv
+> source venv/bin/activate
+> pip install -r requirements.txt
+> export REDCAP_TOKEN_UGH="<token goes here>"
+> python app.py
+```
+
+Then go to http://localhost:5000/ to see the site.
